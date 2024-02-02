3566dff632af5d8547591b224ebfcfba939b5a26
Cleber1812 <cleber1812roberto@gmail.com>
2024-02-02 14:48:19 -0300

    Pg Lancamentos por Empresa

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,4 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { HellowWorld } from "./pages/HellowWorld";
 import { Usuario } from "./pages/Usuario";
@@ -12,8 +8,10 @@ import { DeletarLancamento } from "./pages/LacamentoDeletar";
 import { AtualizarLancamento } from "./pages/LancamentoAtualizar";
 import { Login } from "./pages/Login";
 import { CadastrarUsuario } from "./pages/UsuarioCadastro";
-import { Dashboard } from "./pages/Dashboard";
+import { EmpresasUsuario } from "./pages/EmpresasUsuario";
 import { Home } from "./pages/Home";
+import { LancamentosEmpresa } from "./pages/LancamentosEmpresa";
+import { InserirLancamentoEmpresa } from "./pages/LancamentoInserirPorEmpresa";
 
 export function AppRoutes(){
     return (
@@ -23,12 +21,15 @@ export function AppRoutes(){
                 <Route path="/hellow" element={<HellowWorld />} />                
                 <Route path="/lancamentos" element={<Lancamentos />} />
                 <Route path="/inserirlancamento" element={<InserirLancamento />} />
+                <Route path="/inserirlancamentoempresa" element={<InserirLancamentoEmpresa />} />
                 <Route path="/deletarlancamento" element={<DeletarLancamento />} />
                 <Route path="/atualizarlancamento/:id" element={<AtualizarLancamento />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/cadastrarusuario" element={<CadastrarUsuario />} />
-                <Route path="/minhasempresas/:id" element={<Dashboard />} />
+                {/* <Route path="/minhasempresas" element={<Dashboard />} /> */}
+                <Route path="/minhasempresas/:id" element={<EmpresasUsuario />} />
+                <Route path="/lancamentosempresa/:fk_id_empresa" element={<LancamentosEmpresa />} />
             </Routes>
         </Router>
     )
