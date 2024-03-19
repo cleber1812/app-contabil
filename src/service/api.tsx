6794aebfc059a84bf6eb5016a70fbc757fd1450a
Cleber1812 <cleber1812roberto@gmail.com>
2024-03-19 16:22:19 -0300

    Api online VERCEL

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -3,17 +3,14 @@ import axios from 'axios'
 // const api = axios.create({baseURL: 'http://10.0.2.2:3000/'}) 
 // const api = axios.create({baseURL: 'http://localhost:3306/'}) 
 //const api = axios.create({baseURL: 'http://localhost:3000/'}) 
-//
-
 //const api = axios.create({baseURL: '127.0.0.1:3000/'}) 
-
 //const api = axios.create({baseURL: 'https://desolate-wildwood-71856.herokuapp.com/'}) 
-
 // const api = 'http://localhost:3000';
-
-const api = axios.create({baseURL: 'http://localhost:3000'});
-
 //export default API_BASE_URL;
+
+// const api = axios.create({baseURL: 'http://localhost:3000'});
+const api = axios.create({baseURL: 'https://api-contabil.vercel.app/'}) 
+
 
 // Adiciona um interceptor para incluir o token nas requisições
 api.interceptors.request.use(
