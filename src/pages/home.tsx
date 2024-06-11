34c02e9e53e5acb2c662ce17ff1bbbf920564240
Cleber1812 <cleber1812roberto@gmail.com>
2024-06-11 18:03:52 -0300

    Css LandPage Hero Features Testemunials CalltoAction

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import HeaderLandpage from '../components/HeaderLandpage';
 import Footer from '../components/Footer';
-// import logomarca2 from '../../src/assets/logomarca2.png'; // Importe sua imagem
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIndustry, faListOl, faPieChart } from '@fortawesome/free-solid-svg-icons'; // Exemplo de ícone
+import imgMaicon from '../../src/assets/img-maicon.jpg'; // Importe sua imagem
+import imgElizangela from '../../src/assets/img-elizangela.jpg'; // Importe sua imagem
+import imgPedro from '../../src/assets/img-pedro.jpg'; // Importe sua imagem
 
 export function Home() {
   const navigate = useNavigate();
@@ -19,17 +23,22 @@ export function Home() {
 
         <section id="features" className="features">
           <h2>Funcionalidades</h2>
-          <div className="feature-item">
-            <h3>Registro de Empresas</h3>
-            <p>Facilidade para cadastrar múltiplas empresas em poucos passos.</p>
-          </div>
-          <div className="feature-item">
-            <h3>Lançamentos Contábeis</h3>
-            <p>Insira compras, vendas, despesas e muito mais de forma intuitiva.</p>
-          </div>
-          <div className="feature-item">
-            <h3>Visualização de Resultados</h3>
-            <p>Veja o Balanço Patrimonial e DRE em tempo real.</p>
+          <div className="features-container">
+            <div className="feature-item">
+              <FontAwesomeIcon icon={faIndustry} className="feature-icon" />
+              <h3>Registro de Empresas</h3>
+              <p>Facilidade para cadastrar múltiplas empresas em poucos passos.</p>
+            </div>
+            <div className="feature-item">
+              <FontAwesomeIcon icon={faListOl} className="feature-icon" />
+              <h3>Lançamentos Contábeis</h3>
+              <p>Insira compras, vendas, despesas e muito mais de forma intuitiva.</p>
+            </div>
+            <div className="feature-item">
+              <FontAwesomeIcon icon={faPieChart} className="feature-icon" />
+              <h3>Visualização de Resultados</h3>
+              <p>Veja o Balanço Patrimonial e DRE em tempo real.</p>
+            </div>
           </div>
         </section>
 
@@ -55,22 +64,27 @@ export function Home() {
 
         <section id="testimonials" className="testimonials">
           <h2>O Que Nossos Usuários Dizem</h2>
-          <div className="testimonial-item">
-            <p>"Contábil123 facilitou meu aprendizado de contabilidade!"</p>
-            <p>- Usuário 1</p>
-          </div>
-          <div className="testimonial-item">
-            <p>"Agora eu consigo entender melhor os relatórios financeiros."</p>
-            <p>- Usuário 2</p>
-          </div>
-          <div className="testimonial-item">
-            <p>"Uma ferramenta essencial para estudantes de administração."</p>
-            <p>- Usuário 3</p>
+          <div className="testimonials-container">
+            <div className="testimonials-item">
+              <img src={imgMaicon} className="testimonials-icon" alt="Logo da empresa" />
+              <p>"Contábil123 facilitou meu aprendizado de contabilidade!"</p>
+              <p>Maicon</p>
+            </div>
+            <div className="testimonials-item">
+              <img src={imgElizangela} className="testimonials-icon" alt="Logo da empresa" />
+              <p>"Agora eu consigo entender melhor os relatórios financeiros."</p>
+              <p>Elizângela</p>
+            </div>
+            <div className="testimonials-item">
+              <img src={imgPedro} className="testimonials-icon" alt="Logo da empresa" />
+              <p>"Uma ferramenta essencial para estudantes de administração."</p>
+              <p>Pedro</p>
+            </div>
           </div>
         </section>
 
         <section className="call-to-action">
-          <h2>Pronto para Começar?</h2>
+          <h2>Vamos Começar?</h2>
           <p>Crie sua conta gratuita e comece a praticar hoje mesmo.</p>
           <button onClick={() => navigate('/cadastrarusuario')}>Cadastre-se Gratuitamente</button>
         </section>
